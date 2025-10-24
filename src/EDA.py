10173fcfc31d549a7bdb7deb55f74a1863fc240a
Audrey Stemen <stemenau@gateway-01.dmz>
2025-10-24 19:26:35 -0400

    added header and modified plot formats

--- src/EDA.py
+++ src/EDA.py
@@ -1,3 +1,13 @@
+"""
+Module for exploratory data analysis (EDA) of a dataset.
+
+This module contains functions to perform various EDA tasks such as
+plotting histograms, pairplots, and Spearman correlation matrices.
+The module outputs a PNG for each type of analysis.
+
+Author: Audrey Stemen (stemenau@msu.edu)
+Date: October 2025
+"""
 # Import necessary packages
 import pandas as pd
 import seaborn as sns
@@ -6,30 +16,38 @@ import matplotlib.pyplot as plt
 # Load dataset
 def load_data(csv):
     data = pd.read_csv(csv)
+    # Exclude 'Canonical SMILES' column for numerical analysis
+    data = data.select_dtypes(include=['number'])
     return data
 
 # Histograms of feature values - visualize distributions of each feature
 def plot_histograms(data):
-    data.hist(bins=30, figsize=(15, 10))
+    fig, ax = plt.subplots(3, 3, figsize=(15, 15))
+    n_cols = len(data.columns)
+    colors = sns.color_palette("rocket", n_colors=n_cols)
+    for i, col in enumerate(data.columns):
+        sns.histplot(data[col], bins=30, color=colors[i], ax=ax[i // 3, i % 3])
     plt.tight_layout()
-    plt.savefig('../results/samples_histograms.png')
+    plt.savefig('histograms.png')
 
 # Pairplots - visually identify any relationships between features
 def plot_pairplots(data):
     sns.pairplot(data)
-    plt.savefig('../results/samples_pairplots.png')
+    plt.savefig('pairplots.png')
 
 # Spearman correlation matrix - identify monotonic relationships between features
 def plot_spearman_correlation(data):
     corr = data.corr(method='spearman')
     plt.figure(figsize=(12, 10))
-    sns.heatmap(corr, annot=True, fmt=".2f", cmap='coolwarm')
+    sns.heatmap(corr, annot=True)
     plt.title('Spearman Correlation Matrix')
-    plt.savefig('../results/samples_spearman_correlation.png')
+    plt.savefig('spearman_correlation.png')
 
-# Main function to run EDA
 def main(): 
-    data = load_data('pfas_samples_final.csv')
+    data = load_data('data_complete.csv')
     plot_histograms(data)
     plot_pairplots(data)
     plot_spearman_correlation(data)
+
+if __name__ == "__main__":
+    main()
